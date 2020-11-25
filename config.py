557eb5c8446f5f582b488aebffb8039328281cb6
Nickatak <nickle87@gmail.com>
2020-11-25 15:12:33 -0800

    Add template directory

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+
 class DevConfig(object):
     # It's very unfortunate that you can't enable development-mode without setting Env variables.
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    TEMPLATE_DIR = Path(__file__).parent.joinpath('templates')
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///blah.sqlite3'
