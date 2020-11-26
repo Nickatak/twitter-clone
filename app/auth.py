5f67f2a4a1947dfdfd4e40dfdeb25f9ce1836e6b
Nickatak <nickle87@gmail.com>
2020-11-26 12:38:13 -0800

    Add login route

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-
+# TODO: Remove the url_prefix after we're done making all the routes.
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth_bp.route('/')
@@ -8,3 +8,9 @@ def index():
     """Unauthorized user splash page."""
 
     return render_template('auth/index.html')
+
+@auth_bp.route('/login')
+def login():
+    """Unauthorized user login page."""
+
+    return render_template('auth/login.html')
