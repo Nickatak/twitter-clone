72f1c782a2c09af9f9cf2f17337988e86f2a2565
Nickatak <nickle87@gmail.com>
2020-11-27 14:22:49 -0800

    Add twitter app

---
+++ app/twitter/__init__.py
@@ -0,0 +1,1 @@
+from app.twitter.blueprints import *
---
+++ app/twitter/blueprints.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, g, render_template, redirect, session, url_for
+from app.auth.models import User
+
+twitter_bp = Blueprint('twitter', __name__)
+
+@twitter_bp.before_request
+def assign_uid():
+    if 'uid' in session:
+        g.uid = session['uid']
+    else:
+        g.uid = None
+
+
+@twitter_bp.route('/home')
+def dashboard():
+    """Twitter Clone Dashboard.
+        TODO: Implement this.
+    """
+
+    # Simple unauthorized redirect.
+    if g.uid is None:
+        return redirect(url_for('auth.login'))
+
+    return render_template('twitter/dashboard.html')
