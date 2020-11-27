c764e804703e2f0179a6cb9d9885462b091eac63
Nickatak <nickle87@gmail.com>
2020-11-27 11:52:25 -0800

    Add login route

--- app/auth/blueprints.py
+++ app/auth/blueprints.py
@@ -1,17 +1,20 @@
 """Auth routes."""
 from flask import Blueprint, render_template, session
 
+from app.auth.forms import LoginForm
+from app.auth.models import User
+
 # TODO: Remove the url_prefix after we're done making all the routes.
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-def login(user):
+def do_login(user):
     """Helper function to log a user in by setting the primary key (`id`) of a user in a session.
         :user: app.auth.models.User instance, so we can grab the primary key and set it into session.
     """
 
     session['uid'] = user.id
 
-def logout():
+def do_logout():
     """Helper function to logout a user by clearing all session keys."""
 
     session.clear()
@@ -23,11 +26,20 @@ def index():
 
     return render_template('auth/index.html')
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     """User login page."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data, form.password.data)
+        if user is not None:
+            do_login(user)
+            # TODO: Add a redirect out to the dashboard page (NYI).
+            return redirect('/')
+        else:
+            form.username.errors.append('The username and password you entered did not match our records. Please double-check and try again.')
 
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', form=form)
 
 @auth_bp.route('/signup')
 def signup():
