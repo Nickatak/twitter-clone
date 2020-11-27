c8a8854bb91637c46e0f26673d010c967043820c
Nickatak <nickle87@gmail.com>
2020-11-27 11:16:53 -0800

    Add login/logout helpers

--- app/auth/blueprints.py
+++ app/auth/blueprints.py
@@ -1,8 +1,21 @@
 """Auth routes."""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 # TODO: Remove the url_prefix after we're done making all the routes.
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+def login(user):
+    """Helper function to log a user in by setting the primary key (`id`) of a user in a session.
+        :user: app.auth.models.User instance, so we can grab the primary key and set it into session.
+    """
+
+    session['uid'] = user.id
+
+def logout():
+    """Helper function to logout a user by clearing all session keys."""
+
+    session.clear()
+
 
 @auth_bp.route('/')
 def index():
