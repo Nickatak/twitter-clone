a5ddf249b8bb4ba4df7488fe270d3921ba4339d7
Nickatak <nickle87@gmail.com>
2020-11-27 13:39:12 -0800

    Remove phone number temporarily

--- app/auth/models.py
+++ app/auth/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     username = db.Column(db.String(15), unique=True)
 
     # Going to err on the side of caution here, as India's phone numbers can be 13 digits long (and I'm not sure how the future will go). TODO: Replace this with PhoneNumberField.
-    phone = db.Column(db.String(30), unique=True)
+    #phone = db.Column(db.String(30), unique=True)
     # Going to set an arbitrary length here, I think that this is pretty reasonable for humans and should deny extremely long auto-generated emails.  Of course, additional validation still needs to be applied.
     email = db.Column(db.String(100), unique=True)
 
