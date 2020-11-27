d5216be3c439943bd6be2352b7f71359d534f093
Nickatak <nickle87@gmail.com>
2020-11-27 09:52:28 -0800

    Add testing config

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,10 @@ import sys
 # I have to add the app (package) on Python's PATH before create_app can be imported and the tests can be ran.
 sys.path.append(str(Path(__file__).parent.parent))
 
-from app import create_app
-flask_app = create_app()
+from app import create_app, db
+from config import TestConfig
+
+flask_app = create_app(TestConfig)
 
 @pytest.fixture
 def app():
