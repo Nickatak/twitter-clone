119fbca2f7f18bda9f76b85b56b5242728b1b3dd
Nickatak <nickle87@gmail.com>
2020-11-27 10:20:13 -0800

    Make email field unique

--- app/auth/models.py
+++ app/auth/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     # Going to err on the side of caution here, as India's phone numbers can be 13 digits long (and I'm not sure how the future will go). TODO: Replace this with PhoneNumberField.
     phone = db.Column(db.String(30), unique=True)
     # Going to set an arbitrary length here, I think that this is pretty reasonable for humans and should deny extremely long auto-generated emails.  Of course, additional validation still needs to be applied.
-    email = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
 
     # ALthough things like SHA512 use 128 chars, and Bcrypt uses somewhere about 64 (max) chars depending upon implementation, I figured we'd be safer by just having a larger storage container for future hash implementations.
     password = db.Column(db.String(256))
