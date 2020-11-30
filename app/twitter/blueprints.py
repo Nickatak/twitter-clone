86e6d3c66e8448b4173ff69aa5ad86584ce80238
Nickatak <nickle87@gmail.com>
2020-11-29 20:21:19 -0800

    Add minimum-working posts

--- app/twitter/blueprints.py
+++ app/twitter/blueprints.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, g, render_template, redirect, session, url_for
+from flask import Blueprint, g, render_template, redirect, request, session, url_for
+
+from app import db
+from app.twitter.models import Post
 
 twitter_bp = Blueprint('twitter', __name__)
 
@@ -21,4 +24,29 @@ def dashboard():
     if g.uid is None:
         return redirect(url_for('auth.login'))
 
-    return render_template('twitter/dashboard.html')
+    all_posts = Post.query.all()
+
+
+    return render_template('twitter/dashboard.html', all_posts=all_posts)
+
+@twitter_bp.route('/posts/create', methods=['POST'])
+def create_post():
+    """Create a new POST
+        This is currently here just for minimally-working testing.
+
+        TODO: Implement this.
+    """
+
+    # Simple unauthorized redirect.
+    if g.uid is None:
+        return redirect(url_for('auth.login'))
+
+    new_post = Post(
+        user_id = g.uid,
+        content=request.form['content'],
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('twitter.dashboard'))
--- app/twitter/models.py
+++ app/twitter/models.py
@@ -10,6 +10,7 @@ class Reply(db.Model):
         This serves to create "reply chains" of tweets.
     """
     __tablename__ = 'replies'
+    id = db.Column(db.Integer, primary_key=True)
     target_post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
     reply_post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
 
@@ -26,7 +27,14 @@ class Post(db.Model):
 
     # We'll revise this later to include standard tweet character limits.
     content = db.Column(db.Text)
-    replies = db.relationship('Reply', backref='replies_to', secondary='replies')
+    # Yikes, self-M2M's in SQLAlchemy are kind of difficult and require explicit raw table specification.
+    replies = db.relationship('Reply',
+     secondary=Reply.__table__,
+     primaryjoin=Reply.__table__.columns.target_post_id == id,
+     secondaryjoin=Reply.__table__.columns.target_post_id == id,
+     backref=db.backref('replies', lazy='dynamic'),
+     lazy='dynamic',
+    )
 
 
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
